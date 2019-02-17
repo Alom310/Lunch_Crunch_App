00f048cbfa9031fd89da0a30e7086916adf137df
alom310 <alom310@gmail.com>
2019-02-17 11:50:46 -0800

    added routes

--- models/user.js
+++ models/user.js
@@ -1,13 +1,13 @@
 const mongoose = require('mongoose');
 Schema = mongoose.Schema;
-const Meal = require('./meal');
+// const Meal = require('./meal');
 
 const UserSchema = new Schema({
     name: String,
     budget: Number,
     streak: Number,
-    image: String,
-    meals: [Meal.schema]
+    // image: String,
+    // meals: [Meal.schema]
 });
 
 const User = mongoose.model('User', UserSchema);
