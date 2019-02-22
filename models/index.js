768c65d34bdf6c25593eebb59abe9e9b70f93dc6
trrstnn <edeclarin@gmail.com>
2019-02-21 17:14:22 -0800

    Update your database connection to point to Heroku's database.

--- models/index.js
+++ models/index.js
@@ -2,7 +2,8 @@ const mongoose = require("mongoose");
 const Meal = require("./meal");
 const User = require("./user");
 
-mongoose.connect("mongodb://127.0.0.1/lunchcrunch", {useNewUrlParser: true});
+mongoose.connect( process.env.MONGODB_URI || "mongodb://127.0.0.1/lunchcrunch", {useNewUrlParser: true});
+// mongoose.connect("mongodb://127.0.0.1/lunchcrunch", {useNewUrlParser: true});
 
 exports.Meal = Meal;
 exports.User = User;
